8314085741f379d6160c08c2314cd3be5ed0adae
gustavo do carmo <GustavASD@gmail.com>
2025-07-22 00:08:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'epocacosmeticos.vteximg.com.br',
+      },
+      {
+        protocol: 'https',
+        hostname: 'a-static.mlcdn.com.br',
       }
     ],
   },
